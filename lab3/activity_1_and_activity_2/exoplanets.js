64883c59a8da861594e2c4f6a66d7da96336e684
Angeline Vidhula Jeyachandra <ajeyachandra3@gatech.edu>
2020-09-12 15:22:38 -0400

    Cleaned up lab 3 code and added lab 4

--- lab3/activity_1_and_activity_2/exoplanets.js
+++ lab3/activity_1_and_activity_2/exoplanets.js
@@ -1,31 +1,69 @@
-
 // **** Your JavaScript code goes here ****
+let scaling = data => {
+    // contains all the scaling details
+    this.data = data;
 
-let createAxes = data => {
-    var svg = d3.select('svg');
-
-    var hzdExtent = d3.extent(data, function(d){
+    // Habitual Zone Distance scaling
+    this.hzdExtent = d3.extent(this.data, function(d){
         return +d['habital_zone_distance'];
     });
 
-    var xScale = d3.scaleLinear()
-        .domain(hzdExtent)
+    this.xScale = d3.scaleLinear()
+        .domain(this.hzdExtent)
         .range([100,500]);
-        
-    var xAxis1 = d3.axisBottom(xScale).ticks(11);
 
+    this.xAxis1 = d3.axisBottom(this.xScale).ticks(11);
+    
+    this.xAxis2 = d3.axisTop(this.xScale).ticks(11);
+
+    // Mass scaling
+    this.massExtent = d3.extent(this.data, function(d){
+        return +d['mass'];
+    });
+
+    this.yScale = d3.scaleLog()
+        .domain(this.massExtent)
+        .range([60,660]);
+
+    this.yAxis1 = d3.axisLeft(this.yScale).ticks(10);
+    
+    this.yAxis2 = d3.axisRight(this.yScale).ticks(10);
+
+    // Radius scaling
+    this.radExtent = d3.extent(data, function(d){
+        return +d['radius'];
+    });
+
+    this.radiusScale = d3.scaleSqrt()
+        .domain(this.radExtent)
+        .range([0,20]);
+
+    // Color scaling
+    this.colorScale = d3.scaleQuantize()
+        .domain(this.hzdExtent)
+        .range(['#d64d3f', '#96ac3d', '#208d8d']);
+
+    return this;
+}
+
+let createAxes = data => {
+    let svg = d3.select('svg');
+
+    let scaledData = scaling(data);
+    
+    // Append the 2 X Axes
     svg.append('g')
 		.attr('class', 'x axis')
 		.attr('transform', 'translate(0,35)')
-        .call(xAxis1);
+        .call(scaledData.xAxis1);
         
-    var xAxis2 = d3.axisTop(xScale).ticks(11);
 
     svg.append('g')
             .attr('class', 'x axis')
             .attr('transform', 'translate(0,700)')
-            .call(xAxis2);
+            .call(scaledData.xAxis2);
     
+    // Append X Axes Labels
     svg.append('text')
         .attr('class', 'label')
         .attr('transform','translate(245,23)')
@@ -35,30 +73,19 @@ let createAxes = data => {
         .attr('class', 'label')
         .attr('transform','translate(245,723)')
         .text('Habitual Zone Distance');
-    
 
-    var massExtent = d3.extent(data, function(d){
-        return +d['mass'];
-    });
-
-    var yScale = d3.scaleLog()
-        .domain(massExtent)
-        .range([60,660]);
-
-    var yAxis1 = d3.axisLeft(yScale).ticks(10);
-
+    // Append the 2 Y Axes
     svg.append('g')
 		.attr('class', 'y axis')
 		.attr('transform', 'translate(60, 0)')
-        .call(yAxis1);
-    
-    var yAxis2 = d3.axisRight(yScale).ticks(10);
+        .call(scaledData.yAxis1);
     
     svg.append('g')
-            .attr('class', 'y axis')
-            .attr('transform', 'translate(540, 0)')
-            .call(yAxis2);
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(540, 0)')
+        .call(scaledData.yAxis2);
 
+    // Append the 2 Y Axes Labels
     svg.append('text')
         .attr('class', 'label')
         .attr('transform','translate(20,400) rotate(-90)')
@@ -69,51 +96,31 @@ let createAxes = data => {
         .attr('transform','translate(587,400) rotate(-90)')
         .text('Planet Mass (Relative to Earth)');
 
-    createGraph(data, svg);
+    createGraph(data, scaledData, svg);
 }
 
-let createGraph = (data, svg) => {
-    var hzdExtent = d3.extent(data, function(d){
-        return +d['habital_zone_distance'];
-    });
-    var xScale = d3.scaleLinear()
-        .domain(hzdExtent)
-        .range([100,500]);
-    var massExtent = d3.extent(data, function(d){
-        return +d['mass'];
-    });
-    var yScale = d3.scaleLog()
-        .domain(massExtent)
-        .range([60,660]);
-    var radExtent = d3.extent(data, function(d){
-        return +d['radius'];
-    });
-    var radiusScale = d3.scaleSqrt()
-        .domain(radExtent)
-        .range([0,20]);
-    var colorScale = d3.scaleQuantize()
-        .domain(hzdExtent)
-        .range(['#d64d3f', '#96ac3d', '#208d8d']);
-
+let createGraph = (data, scaledData, svg) => {
+    // Create the circles
     svg.append('g')
         .selectAll("dot")
         .data(data)
         .enter()
         .append("circle")
             .attr("r", function(d) {
-                return radiusScale(d.radius);
+                return scaledData.radiusScale(d.radius);
             })
             .attr("cx", function(d) {
-                return xScale(d.habital_zone_distance);
+                return scaledData.xScale(d.habital_zone_distance);
             })
             .attr("cy", function(d) {
-                return yScale(d.mass);
+                return scaledData.yScale(d.mass);
             })
             .attr("fill", function(d) {
-                return colorScale(d.habital_zone_distance);
+                // 3 different color circles
+                return scaledData.colorScale(d.habital_zone_distance);
             });
 
-
+    // Append text for the 4 planets: Mercury, Venus, Earth, Mars
     svg.append('g')
         .selectAll("text")
         .data(data)
@@ -121,10 +128,10 @@ let createGraph = (data, svg) => {
         .filter(function(d) { return d.name == "Mercury"|| d.name == "Earth" || d.name == "Venus" || d.name == "Mars"})
         .append("text")
         .attr("x", function(d) {
-            return xScale(d.habital_zone_distance) + 5;
+            return scaledData.xScale(d.habital_zone_distance) + 5;
         })
         .attr("y", function(d) {
-            return yScale(d.mass) + 5;
+            return scaledData.yScale(d.mass) + 5;
         })
         .text(function (d) {
             return d.name;
